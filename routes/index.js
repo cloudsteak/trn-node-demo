d8d8a6f07215bff55703c4bd1f15a6401cad2233
csjanos1 <129948630+csjanos1@users.noreply.github.com>
2023-05-23 11:16:42 +0200

    Update index.js
    +csj

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   const NodeVersion = process.versions;
-  res.render('index', { data: { nodeVerison: NodeVersion.node, title: "Azure Webapp példa alkalmazás" } });
+  res.render('index', { data: { nodeVerison: NodeVersion.node, title: "Azure Webapp példa alkalmazás csj" } });
 });
 
 module.exports = router;
