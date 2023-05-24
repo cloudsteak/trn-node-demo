7c90f2189746e22a1a0f898669b9818905296b0d
csjanos1 <csjanos77@gmail.com>
2023-05-24 12:55:56 +0200

    init

--- app.js
+++ app.js
@@ -26,7 +26,10 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 //comment
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
