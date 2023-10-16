184b2ec187da4265f12720a7aa75b9d48e566f0d
Kiss, Tibor <tibor.kiss01@sap.com>
2023-10-16 17:01:58 +0200

    lint

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   const NodeVersion = process.versions;
   res.render('index', { data: { nodeVerison: NodeVersion.node, title: "Azure Webapp példa alkalmazás" } });
 });
