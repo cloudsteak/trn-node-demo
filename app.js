5bf3eb52bc40d10a912775b024b32ff88df49be7
Tibor Kiss <support@cloudsteak.com>
2023-10-19 18:43:42 +0200

    hiba elhárítva

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
+  res.status(err.status || 500);
   res.render('error');
 });
 
