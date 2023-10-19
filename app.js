bf9c74a9d4f25ac722c0997f5e29277b918b1d6e
Tibor Kiss <support@cloudsteak.com>
2023-10-19 18:30:21 +0200

    Pontosvessző

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 /* Kezdőlap */
 app.get('/', function (req, res) {
