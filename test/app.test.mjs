3d928ef3041b1a1bf99aa14eda82c799ec25ea2e
the1bit <tibor.kiss@cloudmentor.hu>
2026-04-12 03:34:55 +0200

    Add unit tests for response content and 404 error handling

--- test/app.test.mjs
+++ test/app.test.mjs
@@ -21,4 +21,63 @@ describe('Unit test: Nyitó oldal', () => {
         expect(response.text).to.contain('felhő');
       });
   });
+
+  it('A válasz Content-Type értéke text/html', () => {
+    return request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.headers['content-type']).to.include('text/html');
+      });
+  });
+
+  it('Az oldal tartalmazza az aktuális évet', () => {
+    const currentYear = new Date().getFullYear().toString();
+    return request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.text).to.contain(currentYear);
+      });
+  });
+
+  it('Oldalon szerepel a "NodeJS Verzió:" felirat', () => {
+    return request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.text).to.contain('NodeJS Verzió:');
+      });
+  });
+
+  it('Oldalon szerepel a "Szerver neve:" felirat', () => {
+    return request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.text).to.contain('Szerver neve:');
+      });
+  });
+
+  it('A válasz tartalmaz X-Powered-By fejlécet', () => {
+    return request(app)
+      .get('/')
+      .then((response) => {
+        expect(response.headers).to.have.property('x-powered-by');
+      });
+  });
+});
+
+describe('Unit test: Nem létező oldal', () => {
+  it('Nem létező oldal 404-es hibát ad vissza', () => {
+    return request(app)
+      .get('/nem-letezik')
+      .then((response) => {
+        assert.equal(response.status, 404);
+      });
+  });
+
+  it('A 404-es válasz szövege tartalmaz "Not Found" szöveget', () => {
+    return request(app)
+      .get('/nem-letezik')
+      .then((response) => {
+        expect(response.text).to.contain('Not Found');
+      });
+  });
 });
